ca745f04c964ac9caa920fdfd09c947b095255d9
ibizaSunrise <milalemekh@gmail.com>
2020-12-30 13:07:30 +0100

    Added animations gsap

--- app.js
+++ app.js
@@ -11,3 +11,46 @@ const mobileMenu = () => {
 }  
 
 menu.addEventListener('click', mobileMenu);
+
+// Animations
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.from('.animate-hero', {
+    duration: 0.6,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3
+});
+
+gsap.from('.animate-services', {
+    scrollTrigger: '.animate-services',
+    duration: 0.5,
+    opacity: 1,
+    x: -150,
+    stagger: 0.12
+});
+
+gsap.from('.animate-img', {
+    scrollTrigger: '.animate-services',
+    duration: 1.2,
+    opacity: 0,
+    x: -200,
+});
+
+gsap.from('.animate-membership', {
+    scrollTrigger: '.animate-membership',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.3,
+    delay: 0.5
+});
+
+gsap.from('.animate-card', {
+    scrollTrigger: '.animate-card',
+    duration: 1,
+    opacity: 0,
+    y: -150,
+    stagger: 0.1,
+    delay: 0.2
+});
